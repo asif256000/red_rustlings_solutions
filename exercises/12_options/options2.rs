b9ada7cf1d51f60e9acccc140a6fb1147ace71a0
asif256000 <asif256000@gmail.com>
2023-12-16 13:20:55 +0530

    ✨ 🦀 Options Solved

--- exercises/12_options/options2.rs
+++ exercises/12_options/options2.rs
@@ -17,17 +17,19 @@ mod tests {
     #[test]
     fn layered_option() {
         let range = 10;
-        let mut optional_integers: Vec<Option<i8>> = vec![None];
+        let mut optional_integers: Vec<Option<i8>> = Vec::new();
 
         for i in 1..(range + 1) {
             optional_integers.push(Some(i));
         }
 
+        let mut cursor = range;
+
         // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
         // You can stack `Option<T>`'s into while let and if let
         while let Some(integer) = optional_integers.pop() {
-            assert_eq!(integer, Some(range));
-            range -= 1;
+            assert_eq!(integer, Some(cursor));
+            cursor -= 1;
         }
 
         assert_eq!(cursor, 0);
